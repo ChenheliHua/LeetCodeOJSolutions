79c8489acb08d1e9106cec9957bc39be4bf62e5a
chenhelihua <huachenheli@outlook.com>
2014-10-11 12:53:01 -0500

    Minimum Depth of Binary Tree

---
+++ src/MinimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,41 @@
+package MinimumDepthOfBinaryTree;
+
+import etc.TreeNode;
+
+/**
+ * Author: Chenheli Hua
+ * Email: huachenheli@outlook.com
+ * Date: 10/10/2014
+ */
+public class Solution
+{
+  public int minDepth(TreeNode root)
+  {
+    if (root == null)
+    {
+      return 0;
+    }
+
+    if (root.left == null && root.right == null)
+    {
+      return 1;
+    }
+    else if (root.left == null)
+    {
+      return minDepth(root.right) + 1;
+    }
+    else if (root.right == null)
+    {
+      return minDepth(root.left) + 1;
+    }
+    else
+    {
+      int left = minDepth(root.left);
+      int right = minDepth(root.right);
+
+      int min = (left < right ? left : right) + 1;
+
+      return min;
+    }
+  }
+}
