eeccd3a1c7cd8fe5c443a3a276c1c6903a02aacf
chenhelihua <huachenheli@outlook.com>
2014-11-06 21:41:32 -0600

    Minimum Path Sum

---
+++ src/MinimumPathSum/Solution.java
@@ -0,0 +1,42 @@
+package MinimumPathSum;
+
+/**
+ * Author: Chenheli Hua
+ * Email: huachenheli@outlook.com
+ * Date: 11/6/2014
+ */
+public class Solution
+{
+  public int minPathSum(int[][] grid)
+  {
+    if (grid == null || grid.length == 0 || grid[0].length == 0)
+    {
+      return 0;
+    }
+
+    for (int i = 0; i < grid.length; i++)
+    {
+      for (int j = 0; j < grid[0].length; j++)
+      {
+        if (i == 0 && j == 0)
+        {
+          // Do nothing.
+        }
+        else if (i == 0)
+        {
+          grid[i][j] = grid[i][j] + grid[i][j - 1];
+        }
+        else if (j == 0)
+        {
+          grid[i][j] = grid[i][j] + grid[i - 1][j];
+        }
+        else
+        {
+          grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
+        }
+      }
+    }
+
+    return grid[grid.length - 1][grid[0].length - 1];
+  }
+}
